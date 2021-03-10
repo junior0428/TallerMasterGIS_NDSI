2cda8b34b43a581ca4761b727e26859ec262bb3a
Junior <juniorantoniocalvomontanez@gmail.com>
2021-03-10 17:59:02 -0500

    code

---
+++ PROJECT_R/Code.R
@@ -0,0 +1,22 @@
+#install.packages("sf")
+
+#direccionar la carpeta
+
+#Cargar las imagenes satelitales 
+
+#caragr los shps 
+
+#extraer las badas para el calculo del NDSI
+
+# calculo del NDSI
+
+#visualizacion de los resultados 
+
+#reclasificacion 
+
+#cortar con el area de trabajo 
+
+#convertir raster a vector 
+
+
+#exportar los resultados
